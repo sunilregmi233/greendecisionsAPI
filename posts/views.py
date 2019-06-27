10e1bdf5a79a03d56fe3b9de52148bd6f0eee68e
sunilregmi233 <sunilregmi223@gmail.com>
2019-06-27 22:30:48 +0545

    post api permissions completed

---
+++ posts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+
+from posts.models import Post
+from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
